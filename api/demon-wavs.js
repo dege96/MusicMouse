2e81db5a37d3742938fbace0162297980b4d05b8
dege96 <victordegeer96@gmail.com>
2024-08-21 23:05:15 +0200

    7

--- api/demon-wavs.js
+++ api/demon-wavs.js
@@ -3,9 +3,19 @@
 export default async function handler(req, res) {
     // Example file list for angel-wavs
     const wavFiles = [
-        'sound1.wav',
-        'sound2.wav',
-        // Add all your WAV filenames here
+        'demonsound1.wav',
+        'demonsound2.wav',
+        'demonsound3.wav',
+        'demonsound4.wav',
+        'demonsound5.wav',
+        'demonsound6.wav',
+        'demonsound7.wav',
+        'demonsound8.wav',
+        'demonsound9.wav',
+        'demonsound10.wav',
+        'demonsound11.wav',
+        'demonsound12.wav'
     ];
+    
     res.status(200).json(wavFiles);
 }
